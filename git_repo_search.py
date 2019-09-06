8817b4eb94ad3402c9157a5f5b0d563ccbe8b65c
test <test@test.test>
2019-09-06 15:10:01 +0300

    added field stars_count

--- git_repo_search.py
+++ git_repo_search.py
@@ -5,14 +5,15 @@ from sys import argv
 
 def print_to_csv(out_file,massive):
     open(out_file,'a').writelines('id;name;full_name;html_url;description;fork;created_at;updated_at;pushed_at;homepage;'
-                                  'stargazers_count;has_wiki;has_pages;archived;license;score\n')
+                                  'stargazers_count;has_wiki;has_pages;archived;license;score;stargazers_count\n')
     for i in massive:
         open(out_file,'a').writelines(i+'\n')
 
 def string_to_csv_string(my_dict):
     csv_string=''
     keys=['id', 'name', 'full_name', 'html_url', 'description', 'fork', 'created_at', 'updated_at',
-          'pushed_at', 'homepage', 'stargazers_count','has_wiki', 'has_pages',  'archived',   'license', 'score']
+          'pushed_at', 'homepage', 'stargazers_count','has_wiki', 'has_pages',  'archived',   'license', 'score',
+          'stargazers_count']
     for i in keys:
         csv_string+=(str(my_dict[i])+';')
     return csv_string
@@ -28,13 +29,13 @@ def dicts_to_dictsString(dicts):
 def search_to_git(keyword):
     item_all=set()
 
-    req=get('https://api.github.com/search/repositories?q={}&per_page=100'.format(keyword))
+    req=get('https://api.github.com/search/repositories?q={}&per_page=100&sort=stars'.format(keyword))
     item_all=item_all|dicts_to_dictsString(req.json()['items'])
     page_all=req.json()['total_count']/100
     if page_all>=10:
         page_all=10
     for i in range(2,int(page_all)+1):
-        req = get('https://api.github.com/search/repositories?q={}&per_page=100&page={}'.format(keyword,i))
+        req = get('https://api.github.com/search/repositories?q={}&per_page=100&sort=stars&page={}'.format(keyword,i))
         item_all=item_all|dicts_to_dictsString(req.json()['items'])
 
     return item_all
